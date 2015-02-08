9edea62ab4af958c428b7b4c2b7c215e3d68910b
Martin Atkins <mart@degeneration.co.uk>
2015-02-07 23:53:38 -0800

    Makefile target for building Xilinx .ngc file.
    For the moment this just hard-codes a particular architecture and part,
    which happens to match the one on the Mojo FPGA development board.

--- glue/glue.v
+++ glue/glue.v
@@ -1,4 +1,4 @@
-module top(
+module glue(
    clk,
    reset_n,
    addr_in,
@@ -51,79 +51,19 @@ module top(
    reg [7:0]      supervisor_map_1;
    reg [7:0]      supervisor_map_2;
 
-   localparam     IDLE = 0,
-                  DECODE = 1,
-                  READ = 2,
-                  WRITE = 3,
-                  WAIT = 7;
+   // TODO: External connections for the page table RAM.
+   wire [15:0]    table_ram_addr_bus;
+   wire [15:0]    table_ram_data_bus;
 
-   reg [2:0]      state_d, state_q;
-   reg [3:0]      boot_counter;
+   // TODO: Physical address decoding.
 
-   always @(clk) begin
-      if (reset) begin
-         state_q <= IDLE;
-         boot_counter <= 2'b1000;
-      end else begin
-         state_q <= state_d;
-      end
-
-      case (state_q)
-        IDLE: begin
-           dtack = 0;
-           berr = 0;
-           rom_enable = 0;
-           ram_enable = 0;
-           if (as) begin
-              state_d = DECODE;
-           end
-        end
-
-        DECODE: begin
-           if (boot_counter != 0) begin
-              addr_out = 0;
-              rom_enable = 1;
-           end
-
-           if (read) begin
-              state_d = READ;
-           end else begin
-              state_d = WRITE;
-           end
-        end
-
-        READ: begin
-           if (boot_counter > 0) begin
-              boot_counter = boot_counter >> 1;
-           end
-
-           dtack = 1;
-
-           state_d = IDLE;
-        end
-
-        WRITE: begin
-           if (boot_counter != 0) begin
-              boot_counter = boot_counter - 1;
-           end
-
-           if (rom_enable) begin
-              berr = 1;
-           end else begin
-              dtack = 1;
-           end
-
-           state_d = WAIT;
-        end
-
-        WAIT: begin
-           // Wait for AS to deassert before we return to idle,
-           // signalling the end of the bus cycle.
-           if (~as) begin
-              state_d = IDLE;
-           end
-        end
-      endcase
-   end
-
+   mmu dut(as,
+           addr_in,
+           fc,
+           page_config_select,
+           supervisor_map_1,
+           supervisor_map_2,
+           table_ram_addr_bus,
+           table_ram_data_bus,
+           addr_out);
 endmodule
