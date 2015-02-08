61cf7118a282f9bf1f19e665f3b75ec6c39e8a93
Martin Atkins <mart@degeneration.co.uk>
2015-02-07 20:40:29 -0800

    A fake 68000 CPU implementing a 16-bit read cycle.
    This is intended to mock a 68000 CPU so that the memory/peripheral bus
    controller can more easily be tested. Only a 16-bit read cycle is
    implemented so far, but more to come.

---
+++ glue/fake68k.v
@@ -0,0 +1,147 @@
+/*
+ This module is able to fake the read and write cycles of a 68000-series
+ microcontroller. It is intended only for use in testbenches for our other
+ modules that attempt to communicate with a 68000.
+
+ It's not intended to be synthesizable; it's just for simulation.
+ */
+module fake68k
+   (
+      // Controls for use in test benches.
+      input         clk,
+      input [2:0]   cycle_type,
+      input [23:0]  mock_addr,
+      input [15:0]  mock_data_to_write,
+      input [2:0]   mock_fc,
+      output [15:0] data_read,
+      output [4:0]  mock_state,
+
+      // Simulated 68k device pins to attach to the DUT.
+      input         dtack_n,
+      input         berr_n,
+      output [2:0]  fc,
+      output        read,
+      output [23:0] addr,
+      inout [15:0]  data,
+      output        as_n,
+      output        uds_n,
+      output        lds_n
+   );
+
+   localparam IDLE = 0;
+   localparam READ_CYCLE = 1;
+   localparam WRITE_CYCLE = 2;
+   localparam READ_WRITE_CYCLE = 3;
+
+   wire             dtack = ~dtack_n;
+   wire             berr = ~berr_n;
+
+   reg [4:0]        current_state = 0;
+   reg [4:0]        next_state = 0;
+   reg [15:0]       data_read_reg = 0;
+
+   reg              drive_addr = 0;
+   reg              drive_data = 0;
+   reg              as = 0;
+   reg              uds = 0;
+   reg              lds = 0;
+   reg              write = 0;
+
+   assign data_read = data_read_reg;
+   assign addr = drive_addr ? mock_addr : 'bz;
+   assign data = drive_data ? mock_data_to_write : 'bz;
+   assign as_n = ~as;
+   assign uds_n = ~uds;
+   assign lds_n = ~lds;
+   assign read = ~write;
+   assign fc = cycle_type != 0 ? mock_fc : 'bz;
+
+   assign mock_state = cycle_type != 0 ? current_state : 'bz;
+
+   always @(clk) begin
+      current_state <= cycle_type != 0 ? next_state : 0;
+   end
+
+   always @(*) begin
+      next_state <= current_state;
+      if (cycle_type == READ_CYCLE)
+        case (current_state)
+          0: begin
+             if (cycle_type != 0 && clk == 1)
+               next_state <= 1;
+          end
+          1: next_state <= 2;
+          2: next_state <= 3;
+          3: next_state <= 4;
+          4: begin
+             // Only transition to state 5 once dtack is asserted, and then
+             // only on a falling edge so that state 5 is always a low-clock
+             // cycle.
+             // (Note: We don't support BERR yet.)
+             if (dtack == 1 && clk == 1)
+               next_state <= 5;
+          end
+          5: next_state <= 6;
+          6: next_state <= 7;
+          7: next_state <= 0;
+        endcase
+   end
+
+   always @(current_state) begin
+      // Some defaults to make sure we'll always reset these to *something*
+      // predictable.
+      drive_addr <= 0;
+      drive_data <= 0;
+      as <= 0;
+      uds <= 0;
+      lds <= 0;
+      write <= 0;
+      if (cycle_type == READ_CYCLE)
+        case (current_state)
+          1: begin
+             drive_addr <= 1;
+
+             // Reset data_read_reg just in case it was left populated
+             // by a previous cycle.
+             data_read_reg <= 0;
+          end
+          2: begin
+             drive_addr <= 1;
+             as <= 1;
+             uds <= 1;
+             lds <= 1;
+          end
+          3: begin
+             drive_addr <= 1;
+             as <= 1;
+             uds <= 1;
+             lds <= 1;
+          end
+          4: begin
+             drive_addr <= 1;
+             as <= 1;
+             uds <= 1;
+             lds <= 1;
+          end
+          5: begin
+             drive_addr <= 1;
+             as <= 1;
+             uds <= 1;
+             lds <= 1;
+          end
+          6: begin
+             drive_addr <= 1;
+             as <= 1;
+             uds <= 1;
+             lds <= 1;
+          end
+          7: begin
+             drive_addr <= 1;
+
+             // We must latch the data as we enter S7.
+             data_read_reg <= data;
+          end
+        endcase
+   end
+
+endmodule
---
+++ glue/fake68k_tb.v
@@ -0,0 +1,186 @@
+module fake68k_tb;
+   `include "tap.vh"
+
+   reg [(18*8)-1:0] test_name = 'bz;
+   reg         clk = 0;
+   reg [2:0]   cycle_type = 0;
+   reg [23:0]  mock_addr = 0;
+   reg [15:0]  mock_data_to_write = 0;
+   reg [2:0]   mock_fc = 0;
+   wire [15:0] data_read;
+   wire [4:0]  mock_state;
+   reg         dtack_n = 1;
+   reg         berr_n = 1;
+   wire [2:0]  fc;
+   wire        read;
+   wire [23:0] addr;
+   wire [15:0] data;
+   wire        as_n;
+   wire        uds_n;
+   wire        lds_n;
+
+   reg [15:0]  mock_data_to_read = 0;
+   reg         drive_data_bus = 0;
+   assign data = drive_data_bus ? mock_data_to_read : 'bz;
+
+   fake68k dut(clk,
+               cycle_type,
+               mock_addr,
+               mock_data_to_write,
+               mock_fc,
+               data_read,
+               mock_state,
+               dtack_n,
+               berr_n,
+               fc,
+               read,
+               addr,
+               data,
+               as_n,
+               uds_n,
+               lds_n);
+
+   task test_initial_state;
+      begin
+         #1 test_name <= "Initial State     ";
+         #2;
+         ok(as_n); $display("AS unasserted when idle");
+         ok(uds_n); $display("UDS unasserted when idle");
+         ok(lds_n); $display("LDS unasserted when idle");
+         ok(fc === 3'bz); $display("FCn Z when idle");
+         ok(addr === 24'bz); $display("ADDRn Z when idle");
+         ok(data === 16'bz); $display("DATAn Z when idle");
+         #1 test_name <= 'bz;
+      end
+   endtask
+
+   task test_read_cycle;
+      begin
+         #1 test_name <= "Read Cycle        ";
+
+         // Make sure cycle zero will always start on a positive edge,
+         // by trying to trick the fake68k into starting it on a negative.
+         @(negedge clk);
+         cycle_type = 1; // READ_CYCLE
+         mock_fc = 'b011;
+         mock_addr = 120;
+         dtack_n = 1;
+         berr_n = 1;
+         mock_data_to_read = 'h7f7f;
+         drive_data_bus = 0;
+
+         #1;
+         ok(fc === 'b011); $display("FCn driven when active");
+         ok(mock_state === 0); $display("State is still zero before posedge clk");
+         ok(as_n); $display("AS unasserted in state pre-0");
+         ok(uds_n); $display("UDS unasserted in state pre-0");
+         ok(lds_n); $display("LDS unasserted in state pre-0");
+         ok(addr === 24'bz); $display("ADDRn Z in state pre-0");
+         ok(data === 16'bz); $display("DATAn Z in state pre-0");
+
+         #1;
+         ok(mock_state === 0); $display("State is still zero after posedge clk");
+         ok(fc === 'b011); $display("FCn driven in state 0");
+         ok(as_n); $display("AS unasserted in state 0");
+         ok(uds_n); $display("UDS unasserted in state 0");
+         ok(lds_n); $display("LDS unasserted in state 0");
+         ok(addr === 24'bz); $display("ADDRn Z in state 0");
+         ok(data === 16'bz); $display("DATAn Z in state 0");
+
+         #1;
+         ok(mock_state === 1); $display("State transitions to 1");
+         ok(fc === 'b011); $display("FCn driven in state 1");
+         ok(as_n); $display("AS unasserted in state 1");
+         ok(uds_n); $display("UDS unasserted in state 1");
+         ok(lds_n); $display("LDS unasserted in state 1");
+         ok(addr == 120); $display("ADDRn driven in state 1");
+         ok(data === 16'bz); $display("DATAn Z in state 1");
+
+         #1;
+         ok(mock_state === 2); $display("State transitions to 2");
+         ok(fc === 'b011); $display("FCn driven in state 2");
+         ok(~as_n); $display("AS asserted in state 2");
+         ok(~uds_n); $display("UDS asserted in state 2");
+         ok(~lds_n); $display("LDS asserted in state 2");
+         ok(addr == 120); $display("ADDRn driven in state 2");
+         ok(data === 16'bz); $display("DATAn Z in state 2");
+
+         #1;
+         ok(mock_state === 3); $display("State transitions to 3");
+         ok(fc === 'b011); $display("FCn driven in state 3");
+         ok(~as_n); $display("AS asserted in state 3");
+         ok(~uds_n); $display("UDS asserted in state 3");
+         ok(~lds_n); $display("LDS asserted in state 3");
+         ok(addr == 120); $display("ADDRn driven in state 3");
+         ok(data === 16'bz); $display("DATAn Z in state 3");
+
+         #1;
+         ok(mock_state === 4); $display("State transitions to 4");
+         ok(fc === 'b011); $display("FCn driven in state 4");
+         ok(~as_n); $display("AS asserted in state 4");
+         ok(~uds_n); $display("UDS asserted in state 4");
+         ok(~lds_n); $display("LDS asserted in state 4");
+         ok(addr == 120); $display("ADDRn driven in state 4");
+         ok(data === 16'bz); $display("DATAn Z in state 4");
+
+         #1;
+         ok(mock_state === 4); $display("Still state 4 because no DTACK");
+         dtack_n = 0;
+         drive_data_bus = 1;
+
+         #1;
+         ok(mock_state === 4); $display("Still state 4 because CLK is high");
+
+         #1;
+         ok(mock_state === 5); $display("State transitions to 5");
+         ok(fc === 'b011); $display("FCn driven in state 5");
+         ok(~as_n); $display("AS asserted in state 5");
+         ok(~uds_n); $display("UDS asserted in state 5");
+         ok(~lds_n); $display("LDS asserted in state 5");
+         ok(addr == 120); $display("ADDRn driven in state 5");
+         ok(data === 'h7f7f); $display("DATAn driven by us in state 5");
+
+         #1;
+         ok(mock_state === 6); $display("State transitions to 6");
+         ok(fc === 'b011); $display("FCn driven in state 6");
+         ok(~as_n); $display("AS asserted in state 6");
+         ok(~uds_n); $display("UDS asserted in state 6");
+         ok(~lds_n); $display("LDS asserted in state 6");
+         ok(addr == 120); $display("ADDRn driven in state 6");
+         ok(data === 'h7f7f); $display("DATAn driven by us in state 6");
+         ok(data_read == 0); $display("Data hasn't been latched yet");
+
+         #1;
+         ok(mock_state === 7); $display("State transitions to 7");
+         ok(fc === 'b011); $display("FCn driven in state 7");
+         ok(as_n); $display("AS unasserted in state 7");
+         ok(uds_n); $display("UDS unasserted in state 7");
+         ok(lds_n); $display("LDS unasserted in state 7");
+         ok(addr == 120); $display("ADDRn driven in state 6");
+         ok(data === 'h7f7f); $display("DATAn driven by us in state 7");
+         ok(data_read == 'h7f7f); $display("Data has now been latched");
+         drive_data_bus = 0;
+         dtack_n = 0;
+
+         #1;
+         ok(mock_state === 0); $display("State transitions back to 0");
+         cycle_type = 0; // deactivate fake68k, resetting to intial state
+
+         #1 test_name <= 'bz;
+      end
+   endtask
+
+   initial begin
+      $dumpfile("fake68k_tb.vcd");
+      $dumpvars;
+      $display("1..74");
+
+      test_initial_state();
+
+      test_read_cycle();
+
+      $finish;
+   end
+
+   always #1 clk = ~clk;
+endmodule
