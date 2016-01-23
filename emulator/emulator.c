8f92b470ff8e63cc1c3e3c50f9d7c4f5b6f02029
Martin Atkins <mart@degeneration.co.uk>
2016-01-22 22:48:48 -0800

    Funnel all memory access through a single pair of functions
    Currently these functions are still no-ops but this will become the
    emulation of the memory mapper in the glue logic.

--- emulator/emulator.c
+++ emulator/emulator.c
@@ -13,20 +13,35 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+unsigned char read_memory_byte(unsigned int addr) {
+    printf("Read from 0x%07x\n", addr);
+    return 0;
+}
+
+void write_memory_byte(unsigned int addr, unsigned char val) {
+    printf("Write %02x to 0x%07x\n", val, addr);
+}
+
 unsigned int m68k_read_memory_8(unsigned int logaddr) {
     unsigned int physaddr = mmu_map_addr(logaddr);
-    printf("Read 8 bits at 0x%06x => 0x%07x\n", logaddr, physaddr);
-    return 0;
+    return read_memory_byte(physaddr);
 }
 
 unsigned int m68k_read_memory_16(unsigned int logaddr) {
     unsigned int physaddr = mmu_map_addr(logaddr);
-    printf("Read 16 bits at 0x%06x => 0x%07x\n", logaddr, physaddr);
-    return 0;
+    return (read_memory_byte(physaddr)<<8) | (read_memory_byte(physaddr + 1));
 }
 
 unsigned int m68k_read_memory_32(unsigned int logaddr) {
     unsigned int physaddr = mmu_map_addr(logaddr);
+    return (
+        (read_memory_byte(physaddr)<<24) |
+        (read_memory_byte(physaddr + 1)<<16) |
+        (read_memory_byte(physaddr + 2)<<8) |
+        (read_memory_byte(physaddr + 3))
+    );
+
+    // This old stuff can be removed once I'm done referring to it.
     printf("Read 32 bits at 0x%06x => 0x%07x\n", logaddr, physaddr);
     if (logaddr == 0x4) {
         // TEMP: Fake the initial PC vector for now, until we have
@@ -51,22 +66,32 @@ unsigned int m68k_read_memory_32(unsigned int logaddr) {
 }
 
 void m68k_write_memory_8(unsigned int logaddr, unsigned int val) {
+    unsigned int physaddr = mmu_map_addr(logaddr);
+    write_memory_byte(physaddr, (unsigned char)val);
 }
 
 void m68k_write_memory_16(unsigned int logaddr, unsigned int val) {
+    unsigned int physaddr = mmu_map_addr(logaddr);
+    write_memory_byte(physaddr, (unsigned char)(val>>8));
+    write_memory_byte(physaddr + 1, (unsigned char)val);
 }
 
 void m68k_write_memory_32(unsigned int logaddr, unsigned int val) {
+    unsigned int physaddr = mmu_map_addr(logaddr);
+    write_memory_byte(physaddr, (unsigned char)(val>>24));
+    write_memory_byte(physaddr + 1, (unsigned char)(val>>16));
+    write_memory_byte(physaddr + 2, (unsigned char)(val>>8));
+    write_memory_byte(physaddr + 3, (unsigned char)val);
 }
 
 unsigned int m68k_read_disassembler_8(unsigned int logaddr) {
-    return 0;
+    return m68k_read_memory_8(logaddr);
 }
 
 unsigned int m68k_read_disassembler_16(unsigned int logaddr) {
-    return 0;
+    return m68k_read_memory_16(logaddr);
 }
 
 unsigned int m68k_read_disassembler_32(unsigned int logaddr) {
-    return 0;
+    return m68k_read_memory_32(logaddr);
 }
