8ff02e0982b0381170b5ca7eda0b4ae9fa941de2
Martin Atkins <mart@degeneration.co.uk>
2016-01-22 22:48:01 -0800

    MMU control registers
    Not actually writable yet, but read in the appropriate places so that
    once they are writable they will work.

--- emulator/mmu.c
+++ emulator/mmu.c
@@ -1,17 +1,42 @@
 #include "addrutil.h"
 #include "mmu.h"
+#include <stdint.h>
+
+typedef struct {
+    // If true, the first segment in the supervisor memory map is the
+    // second segment of ROM. Otherwise, it's the first segment of RAM.
+    int rom_at_zero;
+
+    // Segment selectors for the two software-selectable memory segments.
+    uint8_t selectors[2];
+} supervisor_regs;
+
+typedef struct {
+    // Index of the currently-active user page table. Each user process
+    // has its own page table, allowing it to have a different memory map.
+    // Only the low nybble of this byte is significant.
+    uint8_t active_page_table;
+} user_regs;
+
+supervisor_regs supervisor = { 1, {0, 0} };
+user_regs user = { 0 };
 
 unsigned int mmu_map_addr(unsigned int logaddr) {
-    // For the moment this only supports supervisor mode and it
-    // has hard-coded selections for the two selectable memory segments.
+    // For the moment this only supports supervisor mode.
 
     if (MATCH_ADDR(0b00, logaddr, 22)) {
-        // First part of general RAM
-        return MAP_ADDR(0b100000, logaddr, 22);
+        if (supervisor.rom_at_zero) {
+            // Second segment of ROM
+            return MAP_ADDR(0b010001, logaddr, 22);
+        }
+        else {
+            // First segment of general RAM
+            return MAP_ADDR(0b100000, logaddr, 22);
+        }
     }
 
     if (MATCH_ADDR(0b01, logaddr, 22)) {
-        // First part of ROM
+        // First segment of ROM
         return MAP_ADDR(0b010000, logaddr, 22);
     }
 
@@ -33,13 +58,13 @@ unsigned int mmu_map_addr(unsigned int logaddr) {
     if (MATCH_ADDR(0b1010, logaddr, 20)) {
         // Selectable Mapping 1
         // Not yet selectable, so just always return the unmapped memory section
-        return MAP_ADDR(0b00000000, logaddr, 20);
+        return MAP_ADDR(supervisor.selectors[0], logaddr, 20);
     }
 
     if (MATCH_ADDR(0b1011, logaddr, 20)) {
         // Selectable Mapping 2
         // Not yet selectable, so just always return the unmapped memory section
-        return MAP_ADDR(0b00000000, logaddr, 20);
+        return MAP_ADDR(supervisor.selectors[1], logaddr, 20);
     }
 
     if (MATCH_ADDR(0b11, logaddr, 22)) {
