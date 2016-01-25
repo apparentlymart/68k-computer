ba39f6a04ec4b8d1aafa613548932ad2bb5c3749
Martin Atkins <mart@degeneration.co.uk>
2016-01-24 21:25:17 -0800

    Fill out the default vector table

--- os/kernel/boot.c
+++ os/kernel/boot.c
@@ -65,21 +65,93 @@ void boot(void) {
     while (1) { i++; }
 }
 
+void default_bus_error(void) {
+    long int i = 0;
+    while (1) {
+        i = 0xdeadbeef;
+    }
+}
+
 // init_vectors are the vectors that need to be at address 0 while
 // the CPU is initializing.
 // This is not a complete vector table but rather just the first
 // two entries, which is enough to start executing the boot function.
-const void *init_vectors[2] __attribute__((section(".initvectors"))) = {
+const void *init_vectors[3] __attribute__((section(".initvectors"))) = {
     &KERNEL_STACK_BOTTOM, // initial kernel stack pointer
-    &boot // initial program counter
+    &init, // initial program counter
+    &default_bus_error
 };
 
 // vectors are the "real" vectors that will be installed into the
 // exception table by the boot function and then later modified
 // further by the kernel proper
 const void *vectors[256] __attribute__((section(".vectors"))) = {
-    (void*)0, // initial stack pointer not used
-    (void*)0  // initial program counter not used
-    // TODO: remainder of the initial vectors
+    (void*)0, //  0 initial stack pointer not used
+    (void*)0, //  1 initial program counter not used
+    &default_bus_error, //  2 bus error
+    (void*)0, //  3 address error
+    (void*)0, //  4 illegal instruction
+    (void*)0, //  5 division by zero
+    (void*)0, //  6 CHK instruction
+    (void*)0, //  7 TRAPV instruction
+    (void*)0, //  8 privilege violation
+    (void*)0, //  9 trace
+    (void*)0, // 10 unimplemented instruction
+    (void*)0, // 11 unimplemented instruction
+    (void*)0, // 12 reserved
+    (void*)0, // 13 reserved
+    (void*)0, // 14 reserved
+    (void*)0, // 15 uninitialized interrupt vector
+    (void*)0, // 16 reserved
+    (void*)0, // 17 reserved
+    (void*)0, // 18 reserved
+    (void*)0, // 19 reserved
+    (void*)0, // 20 reserved
+    (void*)0, // 21 reserved
+    (void*)0, // 22 reserved
+    (void*)0, // 23 reserved
+    (void*)0, // 24 spurious interrupt
+    (void*)0, // 25 auto-vector 1
+    (void*)0, // 26 auto-vector 2
+    (void*)0, // 27 auto-vector 3
+    (void*)0, // 28 auto-vector 4
+    (void*)0, // 29 auto-vector 5
+    (void*)0, // 30 auto-vector 6
+    (void*)0, // 31 auto-vector 7
+    (void*)0, // 32 TRAP 0
+    (void*)0, // 33 TRAP 1
+    (void*)0, // 34 TRAP 2
+    (void*)0, // 35 TRAP 3
+    (void*)0, // 36 TRAP 4
+    (void*)0, // 37 TRAP 5
+    (void*)0, // 38 TRAP 6
+    (void*)0, // 39 TRAP 7
+    (void*)0, // 40 TRAP 8
+    (void*)0, // 41 TRAP 9
+    (void*)0, // 42 TRAP 10
+    (void*)0, // 43 TRAP 11
+    (void*)0, // 44 TRAP 12
+    (void*)0, // 45 TRAP 13
+    (void*)0, // 46 TRAP 14
+    (void*)0, // 47 TRAP 15
+    (void*)0, // 48 reserved
+    (void*)0, // 49 reserved
+    (void*)0, // 50 reserved
+    (void*)0, // 51 reserved
+    (void*)0, // 52 reserved
+    (void*)0, // 53 reserved
+    (void*)0, // 54 reserved
+    (void*)0, // 55 reserved
+    (void*)0, // 56 reserved
+    (void*)0, // 57 reserved
+    (void*)0, // 58 reserved
+    (void*)0, // 59 reserved
+    (void*)0, // 60 reserved
+    (void*)0, // 61 reserved
+    (void*)0, // 62 reserved
+    (void*)0, // 63 reserved
+
+    // User interrupt vectors
+    (void*)0, // 64 .. 255
 };
 
