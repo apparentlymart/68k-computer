e936d0d726e30710a9ac5bc506082ab2607fab4c
Martin Atkins <mart@degeneration.co.uk>
2016-01-26 01:02:43 -0800

    Add partial stdint.h header to kernel
    This is just the subset of stdint.h I tend to use regularly. To be extended
    as needed.

---
+++ os/kernel/include/stdint.h
@@ -0,0 +1,16 @@
+#ifndef STDINT_H
+#define STDINT_H
+
+// This is not actually a full implementation of stdint.h
+
+typedef char int8_t;
+typedef unsigned char uint8_t;
+typedef int int16_t;
+typedef unsigned int uint16_t;
+typedef long int32_t;
+typedef unsigned long uint32_t;
+
+typedef long intptr_t;
+typedef unsigned long uintptr_t;
+
+#endif
