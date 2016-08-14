214b0b33cfa119f34e0a04fa392c495526392e25
Martin Atkins <mart@degeneration.co.uk>
2016-08-14 00:42:43 -0700

    A still-not-working putpixel helper for the kernel
    Still need to figure out why the video_ram symbol isn't working
    as expected.

--- os/kernel/gfx.h
+++ os/kernel/gfx.h
@@ -1,9 +1,15 @@
 #ifndef GFX_H
 #define GFX_H
 
+#include <stdint.h>
+
 // FIXME: This doesn't work, for some reason.
 extern unsigned short VIDEO_RAM_START;
 //unsigned short *video_ram = &VIDEO_RAM_START;
 unsigned short *video_ram = (unsigned short *)(0x00c00000);
+
+static inline void gfx_putpixel(int x, int y, uint16_t clr) {
+    video_ram[x + (y * 800)] = clr;
+}
 
 #endif
